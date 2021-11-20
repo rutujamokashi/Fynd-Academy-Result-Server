a3d117b4e0ee27876d2697a8d2a9e4a49c710786
rutujamokashi <87315192+rutujamokashi@users.noreply.github.com>
2021-11-20 13:05:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from fastapi_mail import ConnectionConfig
+conf = ConnectionConfig(
+   MAIL_USERNAME='rutujamokashi2021@gmail.com',
+   MAIL_PASSWORD="18!RUTU12kash88",
+   MAIL_FROM = 'rutu181288@gmail.com',
+   MAIL_PORT=587,
+   MAIL_SERVER="smtp.gmail.com",
+   MAIL_TLS=True,
+   MAIL_SSL=False
+)
+
+
+
